e86d12747ffc103faa9c5fcb4a17c77349c7589a
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-11-06 15:56:02 -0300

    initial commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.use(routes);
+
+server.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
+
+server.listen(3333, () => {
+    // eslint-disable-next-line no-console
+    console.log('🟢 Server started on port 3333!');
+});
